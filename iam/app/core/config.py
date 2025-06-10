c931f9ae40e75224151447a16499cc4530b328ad
Peter Gichia <petergichia35@gmail.com>
2025-06-10 20:59:51 +0300

    Feature: Add config declaration variables for the database

--- iam/app/core/config.py
+++ iam/app/core/config.py
@@ -1,6 +1,9 @@
 import secrets
 
-from typing import Annotated, Any, Literal
+from typing import Literal
+
+from pydantic_core import MultiHostUrl
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -12,9 +15,30 @@ class Settings(BaseSettings):
         extra='ignore',
     )
 
+    # Basic app configuration
     PROJECT_NAME: str = 'DeFi Dashboard IAM'
     API_VERSION_STR: str = '/api/v1'
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ENVIRONMENT: Literal['local', 'staging', 'production'] = 'local'
+
+    # Database configuration
+    POSTGRES_SERVER: str
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int = 5432
+
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme='postgresql+psycopg',
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
 
 settings = Settings()
