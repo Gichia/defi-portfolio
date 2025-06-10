9eb83bd216908e16e2567ad20cf4c2df55560e99
Peter Gichia <petergichia35@gmail.com>
2025-06-10 21:05:57 +0300

    Feature: Implement the lifespan function and run prestart file before starting api

--- iam/app/main.py
+++ iam/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
+from app import prestart
 from app.api.main import api_router
 from app.core.config import settings
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    prestart.main()
+    yield
+
+
 app = FastAPI(
+    lifespan=lifespan,
     title=settings.PROJECT_NAME,
     docs_url=f'{settings.API_VERSION_STR}/docs',
     openapi_url=f'{settings.API_VERSION_STR}/openapi.json',
