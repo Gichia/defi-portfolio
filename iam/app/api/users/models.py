4e4d855e10bfe7b45abb5553668399d9b52261ca
Peter Gichia <petergichia35@gmail.com>
2025-06-11 11:36:10 +0300

    Feature: Create a user model file and add some basic definitions of a user

---
+++ iam/app/api/users/models.py
@@ -0,0 +1,17 @@
+from pydantic import EmailStr
+
+from sqlmodel import Field
+
+from app.core.database import SQLModel
+
+
+class UserBase(SQLModel):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str
