f1b798849bd5a5bb3539428763b00ec9369ce355
Peter Gichia <petergichia35@gmail.com>
2025-06-12 10:33:19 +0300

    Feature: Remove the lifespan method since prestart scripts were moved to dockerfile

--- iam/app/main.py
+++ iam/app/main.py
@@ -1,19 +1,10 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 
-from app import prestart
 from app.api.main import api_router
 from app.core.config import settings
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    prestart.main()
-    yield
-
-
 app = FastAPI(
-    lifespan=lifespan,
     title=settings.PROJECT_NAME,
     docs_url=f'{settings.API_VERSION_STR}/docs',
     openapi_url=f'{settings.API_VERSION_STR}/openapi.json',
