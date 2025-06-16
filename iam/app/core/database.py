d90983efb618fbc1fde065a2ec38650e33a7a167
Peter Gichia <petergichia35@gmail.com>
2025-06-16 14:39:22 +0300

    Feature: Implement the function to create the initial superuser

--- iam/app/core/database.py
+++ iam/app/core/database.py
@@ -1,6 +1,25 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import Session, create_engine, select
 
+from app import crud
 from app.core.config import settings
+from app.api.users.models import User
+from app.api.users.schemas import UserCreate
 
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
+
+def init_db(session: Session) -> None:
+    """Initialize the database with the first superuser."""
+
+    user = session.exec(
+        select(User).where(User.email == settings.FIRST_SUPERUSER_EMAIL)
+    ).first()
+    if not user:
+        user_in = UserCreate(
+            email=settings.FIRST_SUPERUSER_EMAIL,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+
+        user = crud.create_user(session=session, user_create=user_in)
