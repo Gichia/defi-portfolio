8c38e117c1741540f53f43ab204fe79d5ff52526
Peter Gichia <petergichia35@gmail.com>
2025-06-17 13:23:26 +0300

    Feature: Create the schemas for the user module

---
+++ iam/app/api/users/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
+from app.api.users.models import UserBase
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=40)
+
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+    full_name: str | None = Field(default=None, max_length=255)
